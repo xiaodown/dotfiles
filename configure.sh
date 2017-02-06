bd4dd87c23c6287ab3a834aa3a2744202736cedf
Brian Lamar <blamarvt@users.noreply.github.com>
2017-02-06 14:13:30 -0500

    Add recursive submodule sync

--- configure.sh
+++ configure.sh
@@ -3,4 +3,4 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle; \
     curl -Sso ~/.vim/autoload/pathogen.vim \
     https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 git submodule update --init
-
+git submodule sync --recursive
